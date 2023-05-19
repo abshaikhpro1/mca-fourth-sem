662fb8354eede344abd8d412d1540b7d31c50a31
abshaikhpro1 <abshaikh.pro1@gmail.com>
2023-05-19 22:58:32 +0530

    ok

--- product/src/main/java/com/product/ProductController.java
+++ product/src/main/java/com/product/ProductController.java
@@ -1,18 +1,17 @@
 package com.product;
 
 import com.product.Product;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@SessionAttributes("productDeleted")
 public class ProductController {
 
     @Autowired
@@ -20,7 +19,8 @@ public class ProductController {
     private List<Product> products = new ArrayList<>();
 
     @GetMapping("/home")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("productDeleted" , false);
         return "home";
     }
 
@@ -83,7 +83,7 @@ public class ProductController {
     @GetMapping("/products/delete")
     public String viewshowDeleteUpdateForm( Model model) {
         model.addAttribute("products", productService.getAllProducts());
-        return "product-list-view-delete.html";
+        return "product-list-view-delete";
     }
 
     @GetMapping("/deleteView/{id}")
@@ -94,9 +94,11 @@ public class ProductController {
     }
 
 
+
     @PostMapping("/delete/{id}")
-    public String deleteProduct(@PathVariable("id") Long id) {
+    public String deleteProduct(@PathVariable("id") Long id, Model model,  HttpServletRequest request) {
         productService.deleteProduct(id);
+        model.addAttribute("productDeleted" , true);
         return "redirect:/products/delete";
     }
 
